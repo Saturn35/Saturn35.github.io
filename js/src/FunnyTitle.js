96ddf084edb3cc312aa576c8ae808502e4be3e78
Saturn35 <huity35@outlook.com>
2020-11-10 15:42:31 +0800

    Site updated: 2020-11-10 15:42:02

---
+++ js/src/FunnyTitle.js
@@ -0,0 +1,17 @@
+<!--浏览器标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ债见！';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ嘻嘻！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
